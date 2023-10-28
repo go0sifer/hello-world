74808331acf181aca3c12f992b5046adb6bd903a
go0se <45025423+go0sifer@users.noreply.github.com>
2023-10-28 14:08:44 -0500

    bugfix
    Fixed bug where passion was true without having both dedication and persistence greater than 0. We require both to be present for passion to be true.

--- shortersuccess.py
+++ shortersuccess.py
@@ -1,16 +1,21 @@
-def success(dedication, persistence, passion):
-    if dedication > 0 and persistence > 0 and passion:
+def success(dedication, persistence):
+    passion = dedication > 0 and persistence > 0
+
+    if passion:
         magic = dedication + persistence
-        return f"You have magic of {magic}" if magic > 0 else "You have no magic."
+        return f"You have a magic rating of {magic}"
     else:
-        return "You have no magic."
+        return "You have no magic. Dedicate yourself and be Persistent. This shows Passion and combined will grant you magic."
 
 # Example usage:
 # dedication = 0
 # persistence = 0
-# passion = True
-# result = success(dedication, persistence, passion)
+# result = success(dedication, persistence)
 
 
-result = success(4, 20, True)
-print(result)
+RESULT = success(4, 20)
+RESULT2 = success(0, 0)
+RESULT3 = success(0, 1)
+print(RESULT)
+print(RESULT2)
+print(RESULT3)
