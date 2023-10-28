1f9ce5270d8d576d93ec2b75f20a5dc32cb0bd7f
go0se <45025423+go0sifer@users.noreply.github.com>
2023-10-28 13:49:15 -0500

    shortedsuccess
    Years later, similar message. AI involved.

---
+++ shortersuccess.py
@@ -0,0 +1,15 @@
+def success(dedication, persistence, passion):
+    if dedication > 0 and persistence > 0 and passion:
+        magic = dedication + persistence
+        return f"You have magic of {magic}" if magic > 0 else "You have no magic."
+    else:
+        return "You have no magic."
+
+# Example usage:
+# dedication = 0
+# persistence = 0
+# passion = True
+
+
+result = success(4, 20, True)
+print(result)
